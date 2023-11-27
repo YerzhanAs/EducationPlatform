8a4070cb4ba0b03cfdb0ac6196df77339c80ade3
YerzhanAs <ashimov.yerzhan@bk.ru>
2023-11-27 15:22:54 +0600

    full version of basic function

--- src/main/java/com/example/authsystemsaas/models/dto/UserUpdateDTO.java
+++ src/main/java/com/example/authsystemsaas/models/dto/UserUpdateDTO.java
@@ -1,6 +1,5 @@
 package com.example.authsystemsaas.models.dto;
 
-import com.example.authsystemsaas.entities.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +7,6 @@ import lombok.Setter;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -29,5 +27,5 @@ public class UserUpdateDTO {
     @NotEmpty(message = "The password  is required.")
     private String password;
 
-    private Set<String> roles;;
+    private Set<String> roles;
 }
